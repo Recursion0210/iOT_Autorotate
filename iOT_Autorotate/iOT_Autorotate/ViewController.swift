d6ba3f8c9e0ae651440be08643adbd49f0e72c7e
huangshuai <768384880@qq.com>
2015-12-28 15:59:11 +0800

    初始化

---
+++ iOT_Autorotate/iOT_Autorotate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOT_Autorotate
+//
+//  Created by 帅帅 on 15/12/28.
+//  Copyright © 2015年 联创中控（北京）科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
